cdc65903e2f3104839130e1be6c5a5ff6aa34b2c
Janus Erasmus <jcerasmus@gmail.com>
2019-12-29 11:19:09 +0200

    Clean driver, add RGB mix animation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -75,12 +75,7 @@ void visInit()
 
 void visHandle()
 {
-	if(ws2812b.transferComplete)
-	{
-		// Update your framebuffer here or swap buffers
-		//visHandle2();
-		ws2812b_handle();
-	}
+	ws2812b_handle();
 }
 
 void paint(uint8_t argc, char **argv)
@@ -121,6 +116,157 @@ void paint(uint8_t argc, char **argv)
 	}
 
 	visHandle();
+}
+
+
+int mix_state = 0;
+int mix_delta = 8;
+int main_delta = 8;
+void animate_mix(int *r, int *g, int *b)
+{
+	 switch(mix_state)
+	 {
+	 //mix green
+	 case 0:
+		 *g += mix_delta;
+		 if((*g > 255) || (*g <= 0))
+		 {
+			 if(*g <= 0)
+				 *g = 0;
+			 if(*g > 255)
+				 *g = 255;
+
+			 mix_delta *= -1;
+
+			 *r = *r + main_delta;
+			 if((*r > 255) || (*r <= 0))
+			 {
+				 if(*r <= 0)
+					 *r = 0;
+				 if(*r > 255)
+					 *r = 255;
+
+				 main_delta *= -1;
+				 mix_state = 1;
+			 }
+		 }
+		 break;
+		 //mix blue
+	 case 1:
+		 *b += mix_delta;
+		 if((*b > 255) || (*b <= 0))
+		 {
+			 if(*b <= 0)
+				 *b = 0;
+			 if(*b > 255)
+				 *b = 255;
+
+			 mix_delta *= -1;
+
+			 *r = *r + main_delta;
+			 if((*r > 255) || (*r <= 0))
+			 {
+				 if(*r <= 0)
+					 *r = 0;
+				 if(*r > 255)
+					 *r = 255;
+
+				 main_delta *= -1;
+				 mix_state = 2;
+			 }
+		 }
+		 break;
+		 //mix red and blue
+	 case 2:
+		 *g += mix_delta;
+		 *b += mix_delta;
+		 if((*g > 255) || (*g <= 0))
+		 {
+			 if(*b <= 0)
+				 *b = 0;
+			 if(*b > 255)
+				 *b = 255;
+
+			 if(*g <= 0)
+				 *g = 0;
+			 if(*g > 255)
+				 *g = 255;
+
+			 mix_delta *= -1;
+
+			 *r = *r + main_delta;
+			 if((*r > 255) || (*r < 0))
+			 {
+				 if(*r <= 0)
+					 *r = 0;
+				 if(*r > 255)
+					 *r = 255;
+
+				 main_delta *= -1;
+				 mix_state = 0;
+			 }
+		 }
+		 break;
+	 }
+}
+
+int row_delta = 1;
+void animate_column_run(int *row)
+{
+	*row += row_delta;
+
+	if(*row > 15)
+	{
+		row_delta *= -1;
+		*row = 14;
+	}
+	if(*row < 0)
+	{
+		row_delta *= -1;
+		*row = 1;
+	}
+}
+
+double radius = 100;
+int red_row = 15;
+int red_col = 0;
+int green_row = 0;
+int green_col = 7;
+int blue_row = 15;
+int blue_col = 15;
+double scale = 1;//0.9;
+
+void get_scale(int row, int col, int *r, int *g, int *b)
+{
+	//red distance (15, 0)
+	int delta_r = red_row - row;
+	int delta_c = red_col - col;
+	int distance = (delta_r *delta_r) + (delta_c * delta_c);
+	double factor = 1.0 - ((double)distance / radius);
+	if(factor < 0)
+		factor = 0;
+	//printf("R (%d, %d), dr: %d, dc %d, d %d, f %f\n", row, col, delta_r, delta_c, distance, factor);
+	*r = factor * 255.0 * scale;
+
+	//green distance (0, 7)
+	delta_r = green_row - row;
+	delta_c = green_col - col;
+	distance = (delta_r *delta_r) + (delta_c * delta_c);
+	factor = 1.0 - ((double)distance / radius);
+	if(factor < 0)
+		factor = 0;
+	//printf("G (%d, %d), dr: %d, dc %d, d %d, f %f\n", row, col, delta_r, delta_c, distance, factor);
+	*g = factor * 255.0 * scale;
+
+	//blue distance (15, 15)
+	delta_r = blue_row - row;
+	delta_c = blue_col - col;
+	distance = (delta_r *delta_r) + (delta_c * delta_c);
+	factor = 1.0 - ((double)distance / radius);
+	if(factor < 0)
+		factor = 0;
+	//printf("B (%d, %d), dr: %d, dc %d, d %d, f %f\n", row, col, delta_r, delta_c, distance, factor);
+	*b = factor * 255.0 * scale;
 }
 
 /* USER CODE END 0 */
@@ -174,12 +320,88 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   terminal_init("art $");
+
+  uint32_t tick = 0;
+  //int row = 0;
+  //int column = 0;
+  int red_delta = 1;
+  int green_delta = 1;
+  int blue_delta = 1;
+  int r = 0,g = 0,b = 0;
+  ws2812b_handle();
+
   while (1)
   {
 	  terminal_run();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  if(tick < HAL_GetTick())
+	  {
+		  tick = HAL_GetTick() + 100;
+		  ws2812b_clear();
+
+//		  animate_mix(&r, &g, &b);
+//
+//		  //printf("(%3d, %3d, %3d)\n", r, g, b);
+//
+//		  for (int row = 0; row < 16; ++row)
+//		  {
+//			  for (int col = 0; col < 16; ++col)
+//			  {
+//				  ws2812b_set_pixel(col, row, r, g, b);
+//			  }
+//		  }
+//
+		  red_row += red_delta;
+		  if(red_row > 15)
+		  {
+			  red_row = 14;
+			  red_delta *= -1;
+		  }
+		  if(red_row < 0)
+		  {
+			  red_row = 1;
+			  red_delta *= -1;
+		  }
+
+		  green_row += green_delta;
+		  if(green_row > 15)
+		  {
+			  green_row = 14;
+			  green_delta *= -1;
+		  }
+		  if(green_row < 0)
+		  {
+			  green_row = 1;
+			  green_delta *= -1;
+		  }
+
+		  blue_row += blue_delta;
+		  if(blue_row > 15)
+		  {
+			  blue_row = 14;
+			  blue_delta *= -1;
+		  }
+		  if(blue_row < 0)
+		  {
+			  blue_row = 1;
+			  blue_delta *= -1;
+		  }
+
+		  for (int row = 0; row < 16; ++row)
+		  {
+			  for (int col = 0; col < 16; ++col)
+			  {
+				  get_scale(row, col, &r, &g, &b);
+				  ws2812b_set_pixel(row, col, r, g, b);
+			  }
+		  }
+//		  ws2812b_set_pixel(red_row, red_col, 255, 0, 0);
+//		  ws2812b_set_pixel(green_row, green_col, 0, 255, 0);
+//		  ws2812b_set_pixel(blue_row, blue_col, 0, 0, 255);
+		  ws2812b_handle();
+	  }
   }
   /* USER CODE END 3 */
 }
--- Core/Src/ws2812.c
+++ Core/Src/ws2812.c
@@ -10,7 +10,7 @@
 
   Licence: MIT License
 
-*/
+ */
 
 #include <string.h>
 #include <stdio.h>
@@ -18,37 +18,59 @@
 #include "stm32f4xx_hal.h"
 #include "ws2812.h"
 
-extern WS2812_Struct ws2812b;
+
+static TIM_HandleTypeDef  TIM1_handle;
+static TIM_OC_InitTypeDef tim2OC1;
+static TIM_OC_InitTypeDef tim2OC2;
+static uint32_t tim_period;
+static uint32_t timer_reset_pulse_period;
+
+static DMA_HandleTypeDef dmaUpdate = {0};
+static DMA_HandleTypeDef dmaCC1 = {0};
+static DMA_HandleTypeDef dmaCC2 = {0};
+#define BUFFER_SIZE		 sizeof(ws2812bDmaBitBuffer)
+
+
+// GPIO enable command
+#define WS2812B_GPIO_CLK_ENABLE() __HAL_RCC_GPIOC_CLK_ENABLE()
+// LED output port
+#define WS2812B_PORT GPIOC
+// LED output pins
+#define WS2812B_PINS (GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7)
 
 // Define source arrays for my DMAs
-uint32_t WS2812_IO_High[] =  { WS2812B_PINS };
-uint32_t WS2812_IO_Low[] = {WS2812B_PINS << 16};
+static const uint32_t WS2812_IO_Low[] = {WS2812B_PINS << 16};
+static const uint32_t WS2812_IO_High[] =  { WS2812B_PINS };
 
-// WS2812 framebuffer - buffer for 2 LEDs - two times 24 bits
-uint8_t ws2812bDmaBitBuffer[24 * 32];
+// This value sets number of periods to generate 50uS Treset signal
+#define WS2812_RESET_PERIOD 50
+
+
+static uint32_t _transferComplete = 1;
+static uint8_t ws2812bDmaBitBuffer[24 * 32];
 
 // Gamma correction table
 const uint8_t gammaTable[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+		0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+		1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+		2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+		5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+		10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+		17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+		25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+		37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+		51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+		69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+		90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+		115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+		144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+		177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+		215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
+};
 
 static void ws2812b_gpio_init(void)
 {
-	// WS2812B outputs
 	WS2812B_GPIO_CLK_ENABLE();
 	GPIO_InitTypeDef  GPIO_InitStruct;
 	GPIO_InitStruct.Pin       = WS2812B_PINS;
@@ -56,28 +78,7 @@ static void ws2812b_gpio_init(void)
 	GPIO_InitStruct.Pull      = GPIO_NOPULL;
 	GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
 	HAL_GPIO_Init(WS2812B_PORT, &GPIO_InitStruct);
-
-	// Enable output pins for debuging to see DMA Full and Half transfer interrupts
-	#if defined(LED_BLUE_PORT) && defined(LED_ORANGE_PORT)
-		__HAL_RCC_GPIOD_CLK_ENABLE();
-
-		GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-
-		GPIO_InitStruct.Pin = LED_BLUE_PIN;
-		HAL_GPIO_Init(LED_BLUE_PORT, &GPIO_InitStruct);
-		GPIO_InitStruct.Pin = LED_ORANGE_PIN;
-		HAL_GPIO_Init(LED_ORANGE_PORT, &GPIO_InitStruct);
-	#endif
 }
-
-TIM_HandleTypeDef    TIM1_handle;
-TIM_OC_InitTypeDef tim2OC1;
-TIM_OC_InitTypeDef tim2OC2;
-
-uint32_t tim_period;
-uint32_t timer_reset_pulse_period;
 
 static void TIM1_init(void)
 {
@@ -128,22 +129,56 @@ static void TIM1_init(void)
 
 }
 
+void DMA_TransferCompleteHandler(DMA_HandleTypeDef *DmaHandle)
+{
+	// Stop timer
+	TIM1->CR1 &= ~TIM_CR1_CEN;
 
+	// Disable DMA
+	__HAL_DMA_DISABLE(&dmaUpdate);
+	__HAL_DMA_DISABLE(&dmaCC1);
+	__HAL_DMA_DISABLE(&dmaCC2);
 
-static DMA_HandleTypeDef     dmaUpdate = {0};
-static DMA_HandleTypeDef     dmaCC1 = {0};
-static DMA_HandleTypeDef     dmaCC2 = {0};
-#define BUFFER_SIZE		768 //sizeof(ws2812bDmaBitBuffer)
+	// Disable the DMA requests
+	__HAL_TIM_DISABLE_DMA(&TIM1_handle, TIM_DMA_UPDATE);
+	__HAL_TIM_DISABLE_DMA(&TIM1_handle, TIM_DMA_CC1);
+	__HAL_TIM_DISABLE_DMA(&TIM1_handle, TIM_DMA_CC2);
 
-uint32_t dummy;
+	// Set 50us period for Treset pulse
+	//TIM2->PSC = 1000; // For this long period we need prescaler 1000
+	TIM1->ARR = timer_reset_pulse_period;
+	// Reset the timer
+	TIM1->CNT = 0;
+
+	// Generate an update event to reload the prescaler value immediately
+	TIM1->EGR = TIM_EGR_UG;
+	__HAL_TIM_CLEAR_FLAG(&TIM1_handle, TIM_FLAG_UPDATE);
+
+	// Enable TIM2 Update interrupt for 50us Treset signal
+	__HAL_TIM_ENABLE_IT(&TIM1_handle, TIM_IT_UPDATE);
+	// Enable timer
+	TIM1->CR1 |= TIM_CR1_CEN;
+
+	// Manually set outputs to low to generate 50us reset impulse
+	WS2812B_PORT->BSRR = WS2812_IO_Low[0];
+}
+
+void DMA_TransferError(DMA_HandleTypeDef *DmaHandle)
+{
+	volatile int i = 0;
+	i++;
+
+	printf("E %lu\n", DmaHandle->ErrorCode);
+}
+
 
 
 static void DMA2_init(void)
 {
-
 	// TIM2 Update event
 	__HAL_RCC_DMA2_CLK_ENABLE();
 
+	dmaUpdate.Instance = DMA2_Stream5;
 	dmaUpdate.Init.Direction = DMA_MEMORY_TO_PERIPH;
 	dmaUpdate.Init.PeriphInc = DMA_PINC_DISABLE;
 	dmaUpdate.Init.MemInc = DMA_MINC_DISABLE;
@@ -158,19 +193,11 @@ static void DMA2_init(void)
 	dmaUpdate.Init.MemBurst = DMA_MBURST_SINGLE;
 	dmaUpdate.Init.PeriphBurst = DMA_PBURST_SINGLE;
 
-	dmaUpdate.Instance = DMA2_Stream5;
-	//dmaUpdate.XferCpltCallback  = TransferComplete;
-	//dmaUpdate.XferErrorCallback = TransferError;
-
-	//HAL_DMA_DeInit(&dmaUpdate);
 	HAL_DMA_Init(&dmaUpdate);
-	//HAL_NVIC_SetPriority(DMA1_Channel2_IRQn, 0, 0);
-	//HAL_NVIC_EnableIRQ(DMA1_Channel2_IRQn);
 	HAL_DMA_Start(&dmaUpdate, (uint32_t)WS2812_IO_High, (uint32_t)(&WS2812B_PORT->BSRR), BUFFER_SIZE);
-	//HAL_DMA_Start(&dmaUpdate, (uint32_t)WS2812_IO_High, (uint32_t)&dummy, BUFFER_SIZE);
-
 
 	// TIM2 CC1 event
+	dmaCC1.Instance = DMA2_Stream1;
 	dmaCC1.Init.Direction = DMA_MEMORY_TO_PERIPH;
 	dmaCC1.Init.PeriphInc = DMA_PINC_DISABLE;
 	dmaCC1.Init.MemInc = DMA_MINC_ENABLE;
@@ -185,21 +212,14 @@ static void DMA2_init(void)
 	dmaCC1.Init.MemBurst = DMA_MBURST_SINGLE;
 	dmaCC1.Init.PeriphBurst = DMA_PBURST_SINGLE;
 
-	dmaCC1.Instance = DMA2_Stream1;
 
-	//dmaUpdate.XferCpltCallback  = TransferComplete;
-	//dmaUpdate.XferErrorCallback = TransferError;
-	//dmaUpdate.XferHalfCpltCallback = TransferHalf;
-	//HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 0, 0);
-	//HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
-	//HAL_DMA_DeInit(&dmaCC1);
 	HAL_DMA_Init(&dmaCC1);
-	HAL_DMA_Start(&dmaCC1, (uint32_t)ws2812bDmaBitBuffer, (uint32_t)(&WS2812B_PORT->BSRR) + 2, BUFFER_SIZE); //BRR
-	//HAL_DMA_Start(&dmaCC1, (uint32_t)ws2812bDmaBitBuffer, (uint32_t)&dummy, BUFFER_SIZE); //BRR
+	HAL_DMA_Start(&dmaCC1, (uint32_t)ws2812bDmaBitBuffer, (uint32_t)(&WS2812B_PORT->BSRR) + 2, BUFFER_SIZE);
 
 	dmaUpdate.XferHalfCpltCallback = DMA_TransferCompleteHandler;
 
 	// TIM2 CC2 event
+	dmaCC2.Instance = DMA2_Stream2;
 	dmaCC2.Init.Direction = DMA_MEMORY_TO_PERIPH;
 	dmaCC2.Init.PeriphInc = DMA_PINC_DISABLE;
 	dmaCC2.Init.MemInc = DMA_MINC_DISABLE;
@@ -214,169 +234,96 @@ static void DMA2_init(void)
 	dmaCC2.Init.MemBurst = DMA_MBURST_SINGLE;
 	dmaCC2.Init.PeriphBurst = DMA_PBURST_SINGLE;
 
-	dmaCC2.Instance = DMA2_Stream2;
 
 	dmaCC2.XferCpltCallback  = DMA_TransferCompleteHandler;
-//	dmaCC2.XferHalfCpltCallback = DMA_TransferHalfHandler;
-
 	dmaCC2.XferErrorCallback = DMA_TransferError;
 
-	//HAL_DMA_DeInit(&dmaCC2);
 	HAL_DMA_Init(&dmaCC2);
 	HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 0, 0);
 	HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
 	HAL_DMA_Start_IT(&dmaCC2, (uint32_t)WS2812_IO_Low, (uint32_t)&WS2812B_PORT->BSRR, BUFFER_SIZE);
-	//HAL_DMA_Start_IT(&dmaCC2, (uint32_t)WS2812_IO_Low, (uint32_t)&dummy, BUFFER_SIZE);
-
-	//__HAL_LINKDMA(&Tim2Handle, hdma,  &dmaCC2);
-
-	memset(ws2812bDmaBitBuffer, 0xff, sizeof(ws2812bDmaBitBuffer));
-
 }
 
-// Transmit the framebuffer
-void ws2812b_handle()
+// Transmit the frame buffer
+int ws2812b_handle()
 {
-	// transmission complete flag
-	ws2812b.transferComplete = 0;
+	int cnt = 1000;
+	while(!_transferComplete && cnt--);
 
-	// clear all DMA flags
-	__HAL_DMA_CLEAR_FLAG(&dmaUpdate, DMA_FLAG_TCIF1_5 | DMA_FLAG_HTIF1_5 | DMA_FLAG_TEIF1_5);
-	__HAL_DMA_CLEAR_FLAG(&dmaCC1, DMA_FLAG_TCIF1_5 | DMA_FLAG_HTIF1_5 | DMA_FLAG_TEIF1_5);
-	__HAL_DMA_CLEAR_FLAG(&dmaCC2, DMA_FLAG_TCIF2_6 | DMA_FLAG_HTIF2_6 | DMA_FLAG_TEIF2_6);
+	if(_transferComplete)
+	{
+		// transmission complete flag
+		_transferComplete = 0;
+
+		// clear all DMA flags
+		__HAL_DMA_CLEAR_FLAG(&dmaUpdate, DMA_FLAG_TCIF1_5 | DMA_FLAG_HTIF1_5 | DMA_FLAG_TEIF1_5);
+		__HAL_DMA_CLEAR_FLAG(&dmaCC1, DMA_FLAG_TCIF1_5 | DMA_FLAG_HTIF1_5 | DMA_FLAG_TEIF1_5);
+		__HAL_DMA_CLEAR_FLAG(&dmaCC2, DMA_FLAG_TCIF2_6 | DMA_FLAG_HTIF2_6 | DMA_FLAG_TEIF2_6);
 
 
-	// configure the number of bytes to be transferred by the DMA controller
-	dmaUpdate.Instance->NDTR = BUFFER_SIZE;
-	dmaCC1.Instance->NDTR = BUFFER_SIZE;
-	dmaCC2.Instance->NDTR = BUFFER_SIZE;
+		// configure the number of bytes to be transferred by the DMA controller
+		dmaUpdate.Instance->NDTR = BUFFER_SIZE;
+		dmaCC1.Instance->NDTR = BUFFER_SIZE;
+		dmaCC2.Instance->NDTR = BUFFER_SIZE;
 
-	// clear all TIM2 flags
-	__HAL_TIM_CLEAR_FLAG(&TIM1_handle, TIM_FLAG_UPDATE | TIM_FLAG_CC1 | TIM_FLAG_CC2 | TIM_FLAG_CC3 | TIM_FLAG_CC4);
+		// clear all TIM2 flags
+		__HAL_TIM_CLEAR_FLAG(&TIM1_handle, TIM_FLAG_UPDATE | TIM_FLAG_CC1 | TIM_FLAG_CC2 | TIM_FLAG_CC3 | TIM_FLAG_CC4);
 
-	// enable DMA channels
-	__HAL_DMA_ENABLE(&dmaUpdate);
-	__HAL_DMA_ENABLE(&dmaCC1);
-	__HAL_DMA_ENABLE(&dmaCC2);
+		// enable DMA channels
+		__HAL_DMA_ENABLE(&dmaUpdate);
+		__HAL_DMA_ENABLE(&dmaCC1);
+		__HAL_DMA_ENABLE(&dmaCC2);
 
-	// IMPORTANT: enable the TIM2 DMA requests AFTER enabling the DMA channels!
-	__HAL_TIM_ENABLE_DMA(&TIM1_handle, TIM_DMA_UPDATE);
-	__HAL_TIM_ENABLE_DMA(&TIM1_handle, TIM_DMA_CC1);
-	__HAL_TIM_ENABLE_DMA(&TIM1_handle, TIM_DMA_CC2);
+		// IMPORTANT: enable the TIM2 DMA requests AFTER enabling the DMA channels!
+		__HAL_TIM_ENABLE_DMA(&TIM1_handle, TIM_DMA_UPDATE);
+		__HAL_TIM_ENABLE_DMA(&TIM1_handle, TIM_DMA_CC1);
+		__HAL_TIM_ENABLE_DMA(&TIM1_handle, TIM_DMA_CC2);
 
-	TIM1->CNT = tim_period-1;
+		TIM1->CNT = tim_period-1;
 
-	// start TIM2
-	__HAL_TIM_ENABLE(&TIM1_handle);
+		// start TIM2
+		__HAL_TIM_ENABLE(&TIM1_handle);
+
+		return 1;
+	}
+
+	return 0;
 }
 
-
-void DMA_TransferError(DMA_HandleTypeDef *DmaHandle)
-{
-	volatile int i = 0;
-	i++;
-
-	printf("E %lu\n", DmaHandle->ErrorCode);
-}
-
-
-
-void DMA_TransferCompleteHandler(DMA_HandleTypeDef *DmaHandle)
-{
-
-	#if defined(LED_ORANGE_PORT)
-		LED_ORANGE_PORT->BSRR = LED_ORANGE_PIN;
-	#endif
-
-		ws2812b.repeatCounter = 0;
-
-		// Stop timer
-		TIM1->CR1 &= ~TIM_CR1_CEN;
-
-		// Disable DMA
-		__HAL_DMA_DISABLE(&dmaUpdate);
-		__HAL_DMA_DISABLE(&dmaCC1);
-		__HAL_DMA_DISABLE(&dmaCC2);
-
-		// Disable the DMA requests
-		__HAL_TIM_DISABLE_DMA(&TIM1_handle, TIM_DMA_UPDATE);
-		__HAL_TIM_DISABLE_DMA(&TIM1_handle, TIM_DMA_CC1);
-		__HAL_TIM_DISABLE_DMA(&TIM1_handle, TIM_DMA_CC2);
-
-		// Set 50us period for Treset pulse
-		//TIM2->PSC = 1000; // For this long period we need prescaler 1000
-		TIM1->ARR = timer_reset_pulse_period;
-		// Reset the timer
-		TIM1->CNT = 0;
-
-		// Generate an update event to reload the prescaler value immediately
-		TIM1->EGR = TIM_EGR_UG;
-		__HAL_TIM_CLEAR_FLAG(&TIM1_handle, TIM_FLAG_UPDATE);
-
-		// Enable TIM2 Update interrupt for 50us Treset signal
-		__HAL_TIM_ENABLE_IT(&TIM1_handle, TIM_IT_UPDATE);
-		// Enable timer
-		TIM1->CR1 |= TIM_CR1_CEN;
-
-		// Manually set outputs to low to generate 50us reset impulse
-		WS2812B_PORT->BSRR = WS2812_IO_Low[0];
-
-
-
-	#if defined(LED_ORANGE_PORT)
-		LED_ORANGE_PORT->BSRR = LED_ORANGE_PIN << 16;
-	#endif
-
-}
 
 void DMA2_Stream2_IRQHandler(void)
 {
-
-	#if defined(LED_BLUE_PORT)
-		LED_BLUE_PORT->BSRR = LED_BLUE_PIN;
-	#endif
-
-	// Check the interrupt and clear flag
-	  HAL_DMA_IRQHandler(&dmaCC2);
-
-	#if defined(LED_BLUE_PORT)
-		LED_BLUE_PORT->BSRR = LED_BLUE_PIN << 16;
-	#endif
+	HAL_DMA_IRQHandler(&dmaCC2);
 }
 
 void TIM1_UP_TIM10_IRQHandler(void)
 {
-	#if defined(LED_ORANGE_PORT)
-		LED_ORANGE_PORT->BSRR = LED_ORANGE_PIN;
-	#endif
-
 	HAL_TIM_IRQHandler(&TIM1_handle);
-
-	#if defined(LED_ORANGE_PORT)
-		LED_ORANGE_PORT->BSRR = LED_ORANGE_PIN << 16;
-	#endif
 }
 
 // TIM2 Interrupt Handler gets executed on every TIM2 Update if enabled
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    ws2812b.timerPeriodCounter = 0;
-    TIM1->CR1 = 0; // disable timer
+	TIM1->CR1 = 0; // disable timer
 
-    // disable the TIM2 Update IRQ
-    __HAL_TIM_DISABLE_IT(&TIM1_handle, TIM_IT_UPDATE);
+	// disable the TIM2 Update IRQ
+	__HAL_TIM_DISABLE_IT(&TIM1_handle, TIM_IT_UPDATE);
 
-    // Set back 1,25us period
-    TIM1->ARR = tim_period;
+	// Set back 1,25us period
+	TIM1->ARR = tim_period;
 
-    // Generate an update event to reload the Prescaler value immediatly
-    TIM1->EGR = TIM_EGR_UG;
-    __HAL_TIM_CLEAR_FLAG(&TIM1_handle, TIM_FLAG_UPDATE);
+	// Generate an update event to reload the Pre-scaler value immediately
+	TIM1->EGR = TIM_EGR_UG;
+	__HAL_TIM_CLEAR_FLAG(&TIM1_handle, TIM_FLAG_UPDATE);
 
-    // set transfer_complete flag
-    ws2812b.transferComplete = 1;
+	// set transfer_complete flag
+	_transferComplete = 1;
 }
 
+void ws2812b_clear()
+{
+	memset(ws2812bDmaBitBuffer, 0xff, sizeof(ws2812bDmaBitBuffer));
+}
 
 
 void ws2812b_set_pixel(uint8_t row, uint16_t column, uint8_t red, uint8_t green, uint8_t blue)
@@ -388,9 +335,9 @@ void ws2812b_set_pixel(uint8_t row, uint16_t column, uint8_t red, uint8_t green,
 		return;
 
 	// Apply gamma
-	red = gammaTable[red];
-	green = gammaTable[green];
-	blue = gammaTable[blue];
+		red = gammaTable[red];
+		green = gammaTable[green];
+		blue = gammaTable[blue];
 
 	int offset = column * 24;
 	int bit = row / 2;
@@ -433,11 +380,24 @@ void ws2812b_set_pixel(uint8_t row, uint16_t column, uint8_t red, uint8_t green,
 void ws2812b_init()
 {
 	ws2812b_gpio_init();
+	ws2812b_clear();
 
 	DMA2_init();
 	TIM1_init();
 
-	// Need to start the first transfer
-	ws2812b.transferComplete = 1;
+//	//set all pixels to the 'error'; on GPIO with first DMA transfer
+//	for (int r = 0; r < 16; ++r) {
+//		for (int c = 0; c < 16; ++c) {
+//			ws2812b_set_pixel(r, c, 0, 0x80, 0);
+//		}
+//	}
+
+	//clear lights but the firs DMA transfer has an error
+	while(!ws2812b_handle())
+		printf("WS2812: Clear\n");
+
+	//clear again to cancel DMA error
+	while(!ws2812b_handle())
+			printf("WS2812: Clear\n");
 }
 
